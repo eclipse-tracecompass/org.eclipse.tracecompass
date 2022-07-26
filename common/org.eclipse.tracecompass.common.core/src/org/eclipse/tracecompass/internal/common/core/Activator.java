bf8a60579a6e077bfaf7d2fa9341e6775112ebcb
Matthew Khouzam <matthew.khouzam@ericsson.com>
2022-07-25 20:02:02 -0400

    common.core: trace system info
    This is useful to compare runs on different machines
    
    Logs:
    * Host name
    * Number of cores
    * Simple performance indicator
    * Amount of ram
    * OS
    
    Change-Id: If31df9b6e135969a892350aa3bfb152723a1aa1b
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass/org.eclipse.tracecompass/+/194871
    Tested-by: Patrick Tasse <patrick.tasse@gmail.com>
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Reviewed-by: Patrick Tasse <patrick.tasse@gmail.com>

--- common/org.eclipse.tracecompass.common.core/src/org/eclipse/tracecompass/internal/common/core/Activator.java
+++ common/org.eclipse.tracecompass.common.core/src/org/eclipse/tracecompass/internal/common/core/Activator.java
@@ -14,7 +14,14 @@
 
 package org.eclipse.tracecompass.internal.common.core;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.eclipse.tracecompass.common.core.TraceCompassActivator;
+import org.eclipse.tracecompass.common.core.log.TraceCompassLog;
+import org.eclipse.tracecompass.common.core.log.TraceCompassLogUtils;
 
 /**
  * Plugin activator
@@ -41,7 +48,36 @@ public class Activator extends TraceCompassActivator {
 
     @Override
     protected void startActions() {
-        // Do nothing
+        /* Trace system specs */
+        Logger logger = TraceCompassLog.getLogger(getClass());
+        int nbCpus = Runtime.getRuntime().availableProcessors();
+        long memory = Runtime.getRuntime().totalMemory();
+        String os = System.getProperty("os.name"); //$NON-NLS-1$
+        String hostName = "Unknown"; //$NON-NLS-1$
+        try {
+            InetAddress localMachine = InetAddress.getLocalHost();
+            hostName = localMachine.getHostName();
+        } catch (UnknownHostException e) {
+            // ignore.
+        }
+        /*
+         * Simple benchmark to test floating point math. Should give a decent
+         * idea of the system performance. (one multiplication, one division and a branch)
+         */
+        double data = 1e8 - 7; // random number
+        double otherData = 123.321;
+        long start = System.nanoTime();
+        for (int i = 0; i < 1000000; i++) {
+            data /= otherData;
+            data *= otherData;
+            if (data < 0) {
+                // should not happen
+                throw new IllegalStateException("Floating point error on CPU!"); //$NON-NLS-1$
+            }
+        }
+        long end = System.nanoTime();
+        long bogoMips = (long) (1e12 / (end - start));
+        TraceCompassLogUtils.traceInstant(logger, Level.SEVERE, PLUGIN_ID, "HostName", hostName, "OS", os, "Nb Processors", nbCpus, "BogoMips", bogoMips, "Memory", memory); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
     }
 
     @Override
