9a6d5a07e68ada389606017ad1b6b20cb81aa03b
Marco Miller <marco.miller@ericsson.com>
2022-12-29 15:49:09 -0500

    analysis: Initialize a new 'callstack' plugin core
    Base the new minimal set of plugin files on sibling profiling.core ones.
    
    This new plugin is to incrementally hold the callstack implementation to
    bring in from Incubator. Goal being, to main-line the latter's callstack
    in Trace Compass, as an improved implementation over the current, more
    limited one.
    
    Scaffold the incubated callstack alongside the 'profiling' one this way,
    before potentially deprecating the latter once proven as a subset.
    
    When run in Eclipse, ActivatorTest's getDefault and getAbsoluteFilePath
    methods are not covered locally. Keep them still to match with
    profiling.core's own lack of coverage for those, consistently, for this
    change scope.
    
    Change-Id: Iaad2fd837c3d58f4b6c6a0514dfdd7cfd0d5cee7
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass/org.eclipse.tracecompass/+/197869
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.callstack.core/src/org/eclipse/tracecompass/internal/analysis/callstack/core/Activator.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.internal.analysis.callstack.core;
+
+import org.eclipse.tracecompass.common.core.TraceCompassActivator;
+
+/**
+ * Activator for this plugin
+ */
+public class Activator extends TraceCompassActivator {
+
+    /**
+     * The plug-in ID
+     */
+    public static final String PLUGIN_ID = "org.eclipse.tracecompass.analysis.callstack.core"; //$NON-NLS-1$
+
+    /**
+     * The constructor
+     */
+    public Activator() {
+        super(PLUGIN_ID);
+    }
+
+    /**
+     * Returns the instance of this plug-in
+     *
+     * @return The plugin instance
+     */
+    public static TraceCompassActivator getInstance() {
+        return TraceCompassActivator.getInstance(PLUGIN_ID);
+    }
+
+    @Override
+    protected void startActions() {
+        // Do nothing
+    }
+
+    @Override
+    protected void stopActions() {
+        // Do nothing
+    }
+}
