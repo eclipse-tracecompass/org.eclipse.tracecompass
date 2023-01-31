eda2ffeea41b3274e6dcf1390760faee3293825b
Marco Miller <marco.miller@ericsson.com>
2023-01-31 10:24:36 -0500

    callstack: Add initial test classes from Incubator
    Start test-covering this new callstack plugin being brought from
    Incubator. More tests should come then, likely including SWTBot ones.
    
    The included CallStackStateSystemTest happens to cover CallStackSeries.
    
    Next to cover are LttngUstCallStackAnalysis{Requirement}, followed by
    sub-packages classes, still based on Incubator coverage first.
    
    As in Incubator, skip package-info.java for this root package, as it
    introduces many Eclipse build errors with the current implementation.
    
    Have CallStackStateSystemTest import o.e.t.a.profiling.core.callstack's
    CallStackStateProvider instead of Incubator's, unlike its originating
    Incubator counterpart. This is because all is needed for now is this
    link from its javadoc.
    
    Change-Id: Ifba4de6f660f6546420063b42d427f912ff79ea9
    Signed-off-by: Marco Miller <marco.miller@ericsson.com>
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass/org.eclipse.tracecompass/+/199681
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Bernd Hufmann <bernd.hufmann@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

---
+++ analysis/org.eclipse.tracecompass.analysis.callstack.core.tests/src/org/eclipse/tracecompass/analysis/callstack/core/tests/CallStackSegmentStoreTest.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2017 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.analysis.callstack.core.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Iterator;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.tracecompass.analysis.callstack.core.tests.stubs.CallStackAnalysisStub;
+import org.eclipse.tracecompass.segmentstore.core.ISegment;
+import org.eclipse.tracecompass.segmentstore.core.ISegmentStore;
+import org.junit.Test;
+
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
+
+/**
+ * Test the callstack analysis as a segment store
+ *
+ * @author Geneviève Bastien
+ */
+public class CallStackSegmentStoreTest extends CallStackTestBase {
+
+    /**
+     * Test the callstack data using the callstack object
+     */
+    @Test
+    public void testSeriesSegmentStoreIterator() {
+        CallStackAnalysisStub module = getModule();
+        assertNotNull(module);
+
+        ISegmentStore<@NonNull ISegment> segmentStore = module.getSegmentStore();
+        assertNotNull(segmentStore);
+
+        Iterator<@NonNull ISegment> iterator = segmentStore.iterator();
+        assertEquals("Segment store iterator count", 18, Iterators.size(iterator));
+        assertEquals("Segment store size", 18, segmentStore.size());
+        assertFalse(segmentStore.isEmpty());
+    }
+
+    /**
+     * Test the segment store's intersecting query methods
+     */
+    @Test
+    public void testIntersectingSegmentStore() {
+        CallStackAnalysisStub module = getModule();
+        assertNotNull(module);
+
+        ISegmentStore<@NonNull ISegment> segmentStore = module.getSegmentStore();
+        assertNotNull(segmentStore);
+
+        // Test with some boundaries: all elements that start or end at 10
+        // should be included
+        Iterable<@NonNull ISegment> elements = segmentStore.getIntersectingElements(10L);
+        assertEquals("Intersecting 10", 9, Iterables.size(elements));
+
+        elements = segmentStore.getIntersectingElements(10L, 15L);
+        assertEquals("Between 10 and 15", 12, Iterables.size(elements));
+    }
+}
---
+++ analysis/org.eclipse.tracecompass.analysis.callstack.core.tests/stubs/org/eclipse/tracecompass/analysis/callstack/core/tests/stubs/CallStackProviderStub.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2016 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License 2.0 which
+ * accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.analysis.callstack.core.tests.stubs;
+
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.analysis.profiling.core.callstack.CallStackStateProvider;
+import org.eclipse.tracecompass.statesystem.core.statevalue.ITmfStateValue;
+import org.eclipse.tracecompass.statesystem.core.statevalue.TmfStateValue;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
+
+/**
+ * A call stack state provider stub
+ *
+ * @author Geneviève Bastien
+ */
+public class CallStackProviderStub extends CallStackStateProvider {
+
+    private static final String ENTRY = "entry";
+    private static final String EXIT = "exit";
+
+    /**
+     * Constructor
+     *
+     * @param trace
+     *            The trace to run this provider on
+     */
+    public CallStackProviderStub(ITmfTrace trace) {
+        super(trace);
+    }
+
+    @Override
+    public int getVersion() {
+        return 0;
+    }
+
+    @Override
+    public @NonNull CallStackStateProvider getNewInstance() {
+        return new CallStackProviderStub(getTrace());
+    }
+
+    @Override
+    protected boolean considerEvent(@NonNull ITmfEvent event) {
+        return true;
+    }
+
+    @Override
+    protected @Nullable ITmfStateValue functionEntry(@NonNull ITmfEvent event) {
+        String name = event.getName();
+        if (ENTRY.equals(name)) {
+            ITmfEventField field = event.getContent().getField("op");
+            if (field != null) {
+                return TmfStateValue.newValueString((String) field.getValue());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected @Nullable ITmfStateValue functionExit(@NonNull ITmfEvent event) {
+        String name = event.getName();
+        if (EXIT.equals(name)) {
+            ITmfEventField field = event.getContent().getField("op");
+            if (field != null) {
+                return TmfStateValue.newValueString((String) field.getValue());
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected int getProcessId(@NonNull ITmfEvent event) {
+        ITmfEventField field = event.getContent().getField("pid");
+        if (field != null) {
+            return Integer.parseInt((String) field.getValue());
+        }
+        return CallStackStateProvider.UNKNOWN_PID;
+    }
+
+    @Override
+    protected long getThreadId(@NonNull ITmfEvent event) {
+        ITmfEventField field = event.getContent().getField("tid");
+        if (field != null) {
+            return Integer.parseInt((String) field.getValue());
+        }
+        return CallStackStateProvider.UNKNOWN_PID;
+    }
+}
