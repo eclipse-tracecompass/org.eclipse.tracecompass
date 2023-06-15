d780d04200085c16914d50a057e4d755d4be0b99
Arnaud Fiorini <fiorini.arnaud@gmail.com>
2023-06-15 17:17:34 -0400

    callstack.core: Add test for CallStackSeries contains methods
    [Modified] Add tests for contains method in CallStackSegmentStoreTest
    
    Signed-off-by: Arnaud Fiorini <fiorini.arnaud@gmail.com>
    Change-Id: I60300d6add67700b0a2194c2cf7c81fed73d4ebd
    Reviewed-on: https://git.eclipse.org/r/c/tracecompass/org.eclipse.tracecompass/+/202561
    Tested-by: Trace Compass Bot <tracecompass-bot@eclipse.org>
    Tested-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Matthew Khouzam <matthew.khouzam@ericsson.com>
    Reviewed-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- analysis/org.eclipse.tracecompass.analysis.callstack.core.tests/src/org/eclipse/tracecompass/analysis/callstack/core/tests/CallStackSegmentStoreTest.java
+++ analysis/org.eclipse.tracecompass.analysis.callstack.core.tests/src/org/eclipse/tracecompass/analysis/callstack/core/tests/CallStackSegmentStoreTest.java
@@ -14,11 +14,16 @@ package org.eclipse.tracecompass.analysis.callstack.core.tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.tracecompass.analysis.callstack.core.tests.stubs.CallStackAnalysisStub;
+import org.eclipse.tracecompass.analysis.callstack.core.tests.stubs.CalledFunctionStub;
 import org.eclipse.tracecompass.segmentstore.core.ISegment;
 import org.eclipse.tracecompass.segmentstore.core.ISegmentStore;
 import org.junit.Test;
@@ -68,5 +73,64 @@ public class CallStackSegmentStoreTest extends CallStackTestBase {
 
         elements = segmentStore.getIntersectingElements(10L, 15L);
         assertEquals("Between 10 and 15", 12, Iterables.size(elements));
+    }
+
+    /**
+     * Test the segment store's contains method with invalid / fake inputs
+     */
+    @Test
+    public void testContainsSegmentStoreInvalidInput() {
+        CallStackAnalysisStub module = getModule();
+        assertNotNull(module);
+
+        ISegmentStore<@NonNull ISegment> segmentStore = module.getSegmentStore();
+        assertNotNull(segmentStore);
+
+        // Test wrong input
+        assertFalse(segmentStore.contains(null));
+        assertFalse(segmentStore.containsAll(null));
+        assertTrue(segmentStore.containsAll(Collections.emptyList()));
+
+        // Test with fake segments
+        ISegment fakeSegment = new CalledFunctionStub(0L, 1L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(2L, 321321L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(-10L, -2L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(-10L, 10L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(0L, 10L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(32137216321L, 10L);
+        assertFalse(segmentStore.contains(fakeSegment));
+        fakeSegment = new CalledFunctionStub(32137216321L, 32137216322L);
+        assertFalse(segmentStore.contains(fakeSegment));
+    }
+
+    /**
+     * Test the segment store's contains method with correct input
+     */
+    @Test
+    public void testContainsSegmentStore() {
+        CallStackAnalysisStub module = getModule();
+        assertNotNull(module);
+
+        ISegmentStore<@NonNull ISegment> segmentStore = module.getSegmentStore();
+        assertNotNull(segmentStore);
+
+        // Test that it contains what it really contains
+        Iterator<@NonNull ISegment> segmentIterator = segmentStore.iterator();
+        List<ISegment> segments = new ArrayList<>();
+        while (segmentIterator.hasNext()) {
+            ISegment segment = segmentIterator.next();
+            assertTrue(segmentStore.contains(segment));
+            segments.add(segment);
+            assertTrue(segmentStore.containsAll(segments));
+        }
+        while (!segments.isEmpty()) {
+            segments.remove(0);
+            assertTrue(segmentStore.containsAll(segments));
+        }
     }
 }
