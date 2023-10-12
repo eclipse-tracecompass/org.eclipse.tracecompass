6fc76021e55f13cf98ca7ce83a99229f33fc59db
Bernd Hufmann <bernd.hufmann@ericsson.com>
2023-10-12 11:11:20 -0400

    tmf: Add API to retrieve data provider factories from dp manager
    [Added] Add API to retrieve data provider factories from dp manager
    
    Change-Id: I740526b7edc7d2cf9498b9690d0593419423128a
    Signed-off-by: Bernd Hufmann <bernd.hufmann@ericsson.com>

--- tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/dataprovider/DataProviderManager.java
+++ tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/dataprovider/DataProviderManager.java
@@ -34,6 +34,7 @@ import org.eclipse.tracecompass.tmf.core.signal.TmfTraceClosedSignal;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.core.trace.TmfTraceManager;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 
@@ -248,5 +249,31 @@ public class DataProviderManager {
      */
     public <T extends ITmfTreeDataProvider<? extends ITmfTreeDataModel>> boolean removeDataProvider(ITmfTrace trace, T provider) {
         return fInstances.remove(trace, provider);
+    }
+
+    /**
+     * Get all registered data provider factories.
+     *
+     * @return a collection of existing data provider factories
+     * @since 9.4
+     */
+    public synchronized Collection<IDataProviderFactory> getFactories() {
+        return ImmutableList.copyOf(fDataProviderFactories.values());
+    }
+
+    /**
+     * Get a registered data provider factory for a given ID if it exists.
+     *
+     * @param id
+     *            The ID of the data provider factory or a data provider ID of
+     *            form factoryId:secondaryId
+     *
+     * @return the data provider factory for a given Id.
+     * @since 9.4
+     */
+    public synchronized @Nullable IDataProviderFactory getFactory(String id) {
+        String[] ids = id.split(DataProviderConstants.ID_SEPARATOR, 2);
+        String factoryId = ids.length > 1 ? ids[0] : id;
+        return fDataProviderFactories.get(factoryId);
     }
 }
