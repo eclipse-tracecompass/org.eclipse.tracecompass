2745b18a20a90fa624a89b1a3caba9475375b340
Vlad Arama <vladarama2002@gmail.com>
2024-03-25 08:47:47 -0400

    use <= instead of == to catch 0
    Signed-off-by: Vlad Arama <vlad.arama@ericsson.com>

--- tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/histogram/HistogramView.java
+++ tmf/org.eclipse.tracecompass.tmf.ui/src/org/eclipse/tracecompass/tmf/ui/views/histogram/HistogramView.java
@@ -610,7 +610,7 @@ public class HistogramView extends TmfView implements ITmfTimeAligned {
             realStart = fTraceStartTime;
         }
 
-        if (duration == 1) {
+        if (duration <= 1) {
             fTimeSpanControl.setValue(fWindowSpan);
             return;
         }
