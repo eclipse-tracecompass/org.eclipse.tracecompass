32a83ad34cb3276be90095cb7c29f5fef29f9862
Patrick Tasse <patrick.tasse@gmail.com>
2025-03-26 10:09:23 -0400

    tmf: Add hideDataProvider extension point element
    Add hideDataProvider element with idRegex and tracetype attributes to
    the org.eclipse.tracecompass.tmf.core.dataprovider extension point.
    
    Add dataprovider.ini configuration file template to be stored in the
    o.e.tc.tmf.core plug-in state location.
    
    In DataProviderManager methods, filter-out data provider descriptors and
    factories that are hidden by the hideDataProvider element, or the
    dataprovider.ini configuration file, but that are not shown by the
    configuration file. Note that factories cannot be hidden for a specific
    trace type.
    
    Define constant DataProviderConstants.CONFIG_SEPARATOR.
    
    Describe use of CONFIG_SEPARATOR in ITmfDataProviderConfigurator's
    createDataProviderDescriptors() method.
    
    Signed-off-by: Patrick Tasse <patrick.tasse@gmail.com>

--- tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/config/ITmfDataProviderConfigurator.java
+++ tmf/org.eclipse.tracecompass.tmf.core/src/org/eclipse/tracecompass/tmf/core/config/ITmfDataProviderConfigurator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2024 Ericsson
+ * Copyright (c) 2024, 2025 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License 2.0 which
@@ -13,6 +13,7 @@ package org.eclipse.tracecompass.tmf.core.config;
 
 import java.util.List;
 
+import org.eclipse.tracecompass.tmf.core.component.DataProviderConstants;
 import org.eclipse.tracecompass.tmf.core.dataprovider.DataProviderManager;
 import org.eclipse.tracecompass.tmf.core.dataprovider.IDataProviderDescriptor;
 import org.eclipse.tracecompass.tmf.core.exceptions.TmfConfigurationException;
@@ -33,17 +34,21 @@ public interface ITmfDataProviderConfigurator {
     /**
      * Prepares a data provider based on input parameters and returns its
      * corresponding data provider descriptor.
-     *
+     * <p>
      * The input configuration instance will have default parameters (e.g. name,
      * description or sourceTypeId) and custom parameters which are described by
      * the corresponding {@link ITmfConfigurationSourceType#getSchemaFile()} or
      * by the list of
      * {@link ITmfConfigurationSourceType#getConfigParamDescriptors()}.
-     *
+     * <p>
      * The data provider descriptor shall return the parent data provider ID
      * through {@link IDataProviderDescriptor#getParentId()}, as well as the
      * creation configuration through
      * {@link IDataProviderDescriptor#getConfiguration()}.
+     * <p>
+     * The created data provider's ID and any created analysis module's ID should be
+     * appended with {@link DataProviderConstants#CONFIG_SEPARATOR} followed
+     * by the configuration ID.
      *
      * @param trace
      *            The trace (or experiment) instance
