e8f4c6ebe3da8eb32774f60d9d98d40275092057
tuanvtdeptre199 <tuanvtdeptre199@gmail.com>
2026-06-01 15:18:17 +0700

    lttng2.control.ui:Synchronize the start/stop LTTng trace action in Control view with the external trace

--- lttng/org.eclipse.tracecompass.lttng2.control.ui/src/org/eclipse/tracecompass/internal/lttng2/control/ui/views/handlers/StartHandler.java
+++ lttng/org.eclipse.tracecompass.lttng2.control.ui/src/org/eclipse/tracecompass/internal/lttng2/control/ui/views/handlers/StartHandler.java
@@ -15,8 +15,15 @@ package org.eclipse.tracecompass.internal.lttng2.control.ui.views.handlers;
 
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.tracecompass.internal.lttng2.control.core.model.TraceSessionState;
+import org.eclipse.tracecompass.internal.lttng2.control.ui.Activator;
 import org.eclipse.tracecompass.internal.lttng2.control.ui.views.model.impl.TraceSessionComponent;
+import org.eclipse.tracecompass.lttng2.control.ui.views.signals.ExternalTraceStartSignal;
+import org.eclipse.tracecompass.lttng2.control.ui.views.signals.LTTngSessionStartSignal;
+import org.eclipse.tracecompass.lttng2.control.ui.views.signals.LTTngSessionStopSignal;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
+import org.eclipse.tracecompass.tmf.core.signal.TmfSignalManager;
 
 /**
  * <p>
@@ -26,6 +33,20 @@ import org.eclipse.tracecompass.internal.lttng2.control.ui.views.model.impl.Trac
  * @author Bernd Hufmann
  */
 public class StartHandler extends ChangeSessionStateHandler {
+
+    /**
+     * Constructor
+     */
+    public StartHandler() {
+        super();
+        TmfSignalManager.register(this);
+    }
+
+    @Override
+    public void dispose() {
+        TmfSignalManager.deregister(this);
+        super.dispose();
+    }
 
     // ------------------------------------------------------------------------
     // Accessors
@@ -43,5 +64,35 @@ public class StartHandler extends ChangeSessionStateHandler {
     @Override
     public void changeState(TraceSessionComponent session, IProgressMonitor monitor) throws ExecutionException {
         session.startSession(monitor);
+        TmfSignalManager.dispatchSignal(
+                new LTTngSessionStartSignal(session));
+    }
+
+    /**
+     * Handle the external trace start signal
+     * @param signal contains the information of the start external trace
+     */
+    @TmfSignalHandler
+    public void handle(ExternalTraceStartSignal signal) {
+        Display.getDefault().asyncExec(() -> {
+            try {
+                execute(null);
+            } catch (ExecutionException e) {
+                Activator.getDefault().logError("Failed to synchronize with the external trace start signal", e); //$NON-NLS-1$
+            }
+        });
+    }
+
+    /**
+     * Handle the LTTng session stop signal
+     * @param signal contains the information of the stop LTTng session
+     */
+    @TmfSignalHandler
+    public void handle(LTTngSessionStopSignal signal) {
+        Display.getDefault().asyncExec(() -> {
+            if (signal.getSource() instanceof TraceSessionComponent session && !this.fSessions.contains(session)) {
+                this.fSessions.add(session);
+            }
+        });
     }
 }
